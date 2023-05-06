f7688633e783dfd2afc45be87a5bcbec2485822c
sva71 <crockosha@gmail.com>
2023-05-06 14:44:18 +0300

    Auto-generated commit [ci skip]

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import { AppComponent } from "./app/app.component";
-import { provideAnimations } from "@angular/platform-browser/animations";
-
-bootstrapApplication(AppComponent, {
-    providers: [
-        provideAnimations()
-    ],
-}).catch(err => console.log(err));
